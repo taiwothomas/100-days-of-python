0476f693ff0b713dae18b1da21b537bb18d0ee04
Taiwo Thomas <100496634+taiwothomas@users.noreply.github.com>
2022-04-02 09:43:42 +0000

    Band name generator script

---
+++ 100DaysOfPython/BandNameGenerator.py
@@ -0,0 +1,6 @@
+print("What city are you from?\n")
+city = input("> ")
+print("What's your favourite animal?\n")
+animal = input("> ")
+BandName = city+" "+animal
+print(f"Your band name is {BandName}")
