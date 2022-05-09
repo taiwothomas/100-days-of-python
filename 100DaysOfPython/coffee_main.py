063485d2cb2e97054152bd04c7b0806cd19cc4db
Taiwo Thomas <twthms23@gmail.com>
2022-05-09 21:21:54 +0100

    Minor changes to parameter names

--- 100DaysOfPython/coffee_main.py
+++ 100DaysOfPython/coffee_main.py
@@ -10,11 +10,11 @@ cash = MoneyMachine()
 while on:
     choice = input(f"What would you like? {menu.get_items()}: ")
     if choice != "report" and choice != "off":
-        coffee = menu.find_drink(order_name=choice)
-        if coffee_machine.is_resource_sufficient(drink=coffee):
-            payment = cash.make_payment(cost=coffee.cost)
+        coffee = menu.find_drink(choice)
+        if coffee_machine.is_resource_sufficient(coffee):
+            payment = cash.make_payment(coffee.cost)
             if payment:
-                serve = coffee_machine.make_coffee(order=coffee)
+                serve = coffee_machine.make_coffee(coffee)
     elif choice == "report":
         report = {coffee_machine.report(), cash.report()}
     elif choice == "off":
