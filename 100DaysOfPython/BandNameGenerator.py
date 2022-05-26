bc83168c0fe718a147dd2d7825a8b47b0ef0f50f
Taiwo Thomas <100496634+taiwothomas@users.noreply.github.com>
2022-05-26 17:03:52 +0100

    Delete BandNameGenerator.py

--- 100DaysOfPython/BandNameGenerator.py
+++
@@ -1,6 +0,0 @@
-print("What city are you from?")
-city = input("> ")
-print("What's your favourite animal?")
-animal = input("> ")
-BandName = city+" "+animal
-print(f"Your band name is {BandName}")
